024b036776a7d5ca08c82531dd8a326c31232ef3
DDL51 <montodeckja@gmail.com>
2025-05-11 23:01:20 -0300

    Update Zero (3).py

--- Zero (3).py
+++ Zero (3).py
@@ -193,7 +193,7 @@ def main():
                     st.success("Produto excluído com sucesso!")
                     st.experimental_rerun()
 
-    elif modo == "Orçamento":
+    elif modo == "Orçamentos":
         st.subheader("Orçamento para Cliente")
         nome_cliente = st.text_input("Nome do Cliente")
         contato = st.text_input("Contato")
