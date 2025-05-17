9097498919265265098550a1f830c33c09e7c815
DDL51 <montodeckja@gmail.com>
2025-05-16 22:15:18 -0300

    Update Zero (3).py

--- Zero (3).py
+++ Zero (3).py
@@ -156,10 +156,10 @@ def main():
             repasse = st.number_input("Repasse (R$)", min_value=0.0, format="%.2f")
             usinagem = st.number_input("Usinagem (R$)", min_value=0.0, format="%.2f")
             #
-    if st.button("Salvar Produto") and nome.strip():
-        valor_final = base + (base * imposto / 100) + repasse + usinagem
-        aba_produtos.append_row([
-        nome, base, imposto, repasse, usinagem, round(valor_final, 2)
+            if st.button("Salvar Produto") and nome.strip():
+                valor_final = base + (base * imposto / 100) + repasse + usinagem
+                aba_produtos.append_row([
+                nome, base, imposto, repasse, usinagem, round(valor_final, 2)
     ])
     st.success("Produto salvo com sucesso!")
                 
