4d020561b397d152ce06579d9f9dc0e625e19877
DDL51 <montodeckja@gmail.com>
2025-05-18 17:22:39 -0300

    Update Zero (3).py

--- Zero (3).py
+++ Zero (3).py
@@ -171,10 +171,10 @@ def main():
 
         elif tipo == "Produto":
             nome = st.text_input("Nome do Produto")
-    base = st.number_input("Valor Base (R$)", min_value=0.0, format="%.2f")
-    imposto = st.number_input("Imposto (%)", min_value=0.0, format="%.2f")
-    repasse = st.number_input("Repasse (R$)", min_value=0.0, format="%.2f")
-    usinagem = st.number_input("Usinagem (R$)", min_value=0.0, format="%.2f")
+            base = st.number_input("Valor Base (R$)", min_value=0.0, format="%.2f")
+            imposto = st.number_input("Imposto (%)", min_value=0.0, format="%.2f")
+            repasse = st.number_input("Repasse (R$)", min_value=0.0, format="%.2f")
+            usinagem = st.number_input("Usinagem (R$)", min_value=0.0, format="%.2f")
 
     if st.button("Salvar Produto"):
     if nome.strip():  # Verifica se o nome não está vazio
