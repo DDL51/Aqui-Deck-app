45107ea4378a10062839c98b371909a02c0a46ff
DDL51 <montodeckja@gmail.com>
2025-05-18 17:47:38 -0300

    Update Zero (3).py

--- Zero (3).py
+++ Zero (3).py
@@ -17,11 +17,12 @@ SHEET_NAME = "AQUI-DECK"
 
 #Carregar credenciais do secrets.toml
 #credentials_dict = st.secrets["GOOGLE_CREDENTIALS"]
-credentials = Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=scope
-)
-credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+
+#credentials = Credentials.from_service_account_info(
+    ###st.secrets["gcp_service_account"],
+    ###scopes=scope
+###)
+###credentials = service_account.Credentials.from_service_account_info(credentials_dict)
 # -------- AUTENTICAÇÃO GOOGLE --------
 
 def conectar_planilha():
