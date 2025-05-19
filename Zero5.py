c96aaa05cb72a7b772b3ffdd0a0463598cd6bc5b
DDL51 <montodeckja@gmail.com>
2025-05-19 18:13:08 -0300

    Update Zero5.py

--- Zero5.py
+++ Zero5.py
@@ -90,10 +90,10 @@ def main():
         st.subheader("Gerenciamento de Dados")
         st.info("""
         O gerenciamento de **Produtos**, **Fixos**, **Orçamentos** e **Agenda** pode ser feito diretamente na planilha do Google.
-    - Acesse a planilha para:
-   # - Editar valores ou nomes de produtos/serviços
-    #- Corrigir ou remover registros
-    #- Atualizar orçamentos existentes
+    # - Acesse a planilha para:
+    # - Editar valores ou nomes de produtos/serviços
+    # - Corrigir ou remover registros
+    # - Atualizar orçamentos existentes
     #""")
     
     if st.button("Abrir Planilha"):
